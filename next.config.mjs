b63ed2123c8a3ac3cf6320c3e824a551d2b933a6
Franklin Da Silva <franklindasilvabrz@gmail.com>
2025-04-04 07:54:40 +0900

    final version;

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,8 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  experimental: {
-    granularChunks: true, // Reduces large Webpack chunks
-  },
-  webpack: (config) => {
-    config.optimization.splitChunks = {
-      chunks: "all",
-      maxSize: 24400000, // Keep chunks under 24MB
-    };
-    return config;
-  },
+  reactStrictMode: true,
+  output: 'standalone',
 }
 
 export default withPayload(nextConfig)
